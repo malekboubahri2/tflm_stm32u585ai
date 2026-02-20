f371fdce5fec93b33ca18ee996958561949a76da
Malek Boubahri <90895401+malekboubahri2@users.noreply.github.com>
2026-02-20 16:48:10 +0000

    fix(setup): Install newlib

--- .devcontainer/scripts/setup.sh
+++ .devcontainer/scripts/setup.sh
@@ -3,17 +3,18 @@
 export DEBIAN_FRONTEND=noninteractive
 
 #installing dependencies
-apt-get update
-apt-get install -y --no-install-recommends \
+apt-get update && apt-get install -y --no-install-recommends \
 git \
 build-essential \
 cmake \
 gcc-arm-none-eabi \
+libnewlib-arm-none-eabi \
 make \
+usbutils \
 openocd \
 python3-pip \
-
-rm -rf /var/lib/apt/lists/*
+minicom \
+&& rm -rf /var/lib/apt/lists/*
 
 #exporting cmake path
 export PATH="/opt/$(ls /opt | grep cmake | head -n1)/bin:${PATH}"
